eda1987bcfddf1830a806d7cce39d8af7887ff4d
ilya92S <saitovilya92@yandex.ru>
2024-02-12 17:14:52 +0300

    main commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from loader import bot
+
+from telebot.custom_filters import StateFilter
+
+import handlers
+from database.models import create_models
+from utils.set_bot_commands import set_default_commands
+
+if __name__ == '__main__':
+    create_models()
+    bot.add_custom_filter(StateFilter(bot))
+    set_default_commands(bot)
+    bot.infinity_polling()
